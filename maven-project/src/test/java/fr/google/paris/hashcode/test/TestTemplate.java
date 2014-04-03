2f25638700f6327a315326519442d764b2744b7a
Luc CLÉMENT <luc.clement421@gmail.com>
2014-04-03 15:51:57 +0200

    updates full maven project template

---
+++ maven-project/src/test/java/fr/google/paris/hashcode/test/TestTemplate.java
@@ -0,0 +1,14 @@
+package fr.google.paris.hashcode.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTemplate {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

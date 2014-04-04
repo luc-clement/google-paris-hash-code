f92b21f84dfb37d1c9790fd115974f0ca900e6bb
AlexisTerrat <alexisterrat@gmail.com>
2014-04-04 22:05:22 +0200

    classe Alexis -> definePaintOrders TrialRound

--- maven-project/src/main/java/fr/google/paris/hashcode/Alexis.java
+++ maven-project/src/main/java/fr/google/paris/hashcode/Alexis.java
@@ -1,5 +1,42 @@
 package fr.google.paris.hashcode;
 
-public class Alexis {
+import java.util.ArrayList;
 
+import fr.google.paris.hashcode.domain.Surface;
+
+public class Alexis {
+	
+	public static ArrayList<String> definePaintOrders(int s){
+		Surface S;
+		int x, y, xBest, yBest;
+		double t, tBest;
+		ArrayList<String> orders = new ArrayList<String>();
+		x = 0;
+		y = 0;
+		tBest = 0;
+		boolean go = true;
+		while(go){
+			go = false;
+			for(x = 0; x < TrialRound.map.length - (2*s+1); ++x)
+				for(y = 2*s; y < TrialRound.map[0].length - (2*s+1); ++y){
+					S = new Surface(x,y,2*s+1);
+					t = S.tauxRemplissage();
+					if(t > TrialRound.tauxDeRemplissage && t > tBest && t < 1){
+						xBest = x;
+						yBest = y;
+						tBest = t;
+					}
+				}
+			if(tBest > 0){
+				orders.add("PAINTSQ "+xBest+" "+yBest+" "+s);
+				S = new Surface(xBest,yBest,2*s+1);
+				S.Draw();
+				xBest = 0;
+				yBest = 0;
+				tBest = 0;
+				go = true;
+			}
+		}
+		return orders;
+	}
 }

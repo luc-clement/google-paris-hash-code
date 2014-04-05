d5bf697fa0eedf5abd4c74df5d1acfca02f63c2d
Luc CLÉMENT <luc.clement421@gmail.com>
2014-04-05 12:05:58 +0200

    add logger

--- maven-project/src/main/java/fr/google/paris/hashcode/mainRound/Intersection.java
+++ maven-project/src/main/java/fr/google/paris/hashcode/mainRound/Intersection.java
@@ -2,18 +2,29 @@ package fr.google.paris.hashcode.mainRound;
 
 import java.util.HashMap;
 
+import org.apache.log4j.Logger;
+
 public class Intersection {
+	
+	private final static Logger LOGGER = Logger.getLogger(Intersection.class);
 	
 	int id;
 	double latitude;
 	double longitude;
 	HashMap<Integer, Rue> intersectionsJoignables = new HashMap<Integer, Rue>();
-
 	
 	public Intersection(int id, double latitude, double longitude) {
 		this.id = id;
 		this.latitude = latitude;
 		this.longitude = longitude;
+	}
+	
+	public void addIntersectionJoignables(int intersectionId, Rue rue) {
+		if (intersectionsJoignables.containsKey(intersectionId)) {
+			LOGGER.error("Tentative d'ajout d'une intersection joignable déjà enregistrée");
+		} else {
+			intersectionsJoignables.put(intersectionId, rue);
+		}
 	}
 
 }
--- maven-project/src/main/java/fr/google/paris/hashcode/mainRound/Rue.java
+++ maven-project/src/main/java/fr/google/paris/hashcode/mainRound/Rue.java
@@ -1,7 +1,11 @@
 package fr.google.paris.hashcode.mainRound;
 
+import org.apache.log4j.Logger;
+
 public class Rue {
 	
+	private final static Logger LOGGER = Logger.getLogger(Rue.class);
+
 	int departId;
 	int arriveId;
 	int tempsParcours;
